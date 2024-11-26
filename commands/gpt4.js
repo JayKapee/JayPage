c82372eb18a9ffe9ca78bbf21c84821cec8b41b2
JayKapee <arvieserafin@gmail.com>
2024-11-26 20:36:52 +0800

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -1,21 +1,30 @@
-const axios = require('axios');
-const { sendMessage } = require('../handles/sendMessage');
+const axios = require("axios");
+const name = "ai" ;
 
 module.exports = {
-  name: 'gpt4',
-  description: 'Interact with GPT-4o',
-  usage: 'gpt4 [your message]',
-  author: 'coffee',
+  name,
+  description: "Interact with ChatGPT-4o",
+  async run ({ api, event, send, args }){
+    const prompt = args.join(" ");
+    if (!prompt) return send(`Please enter your question! 
 
-  async execute(senderId, args, pageAccessToken) {
-    const prompt = args.join(' ');
-    if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
-
+Example: ${api.prefix + name} what is love?`);
+    send("JayChat is typing... 🔎");
     try {
-      const { data: { result } } = await axios.get(`https://joshweb.click/api/gpt-4o?q=${encodeURIComponent(prompt)}&uid=${senderId}`);
-      sendMessage(senderId, { text: result }, pageAccessToken);
-    } catch {
-      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+    const gpt = await axios.get(`${api.api_josh}/api/gpt-4o`, {
+      params: {
+        q: prompt,
+        uid: event.sender.id
+      }
+    });
+    if (!gpt || !gpt.data.status)
+    throw new Error();
+    send(`${gpt.data.result}
+
+🤖 JayChat by Jay Ar`);
+    } catch(err){
+      send(err.message || err);
+      return;
     }
   }
-};
+  }
