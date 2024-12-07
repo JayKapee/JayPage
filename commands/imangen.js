1bdfc4506f390cdf57f3650b848d64be42399420
JayKapee <arvieserafin@gmail.com>
2024-12-07 12:14:31 +0800

    Create imangen.js

---
+++ commands/imangen.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'imagine',
+  description: 'generates an image based on prompt',
+  author: 'Jay Ar',
+  async execute(senderId, args, pageAccessToken) {
+    if (!args || !Array.isArray(args) || args.length === 0) {
+      await sendMessage(senderId, { text: 'Please provide a prompt for image generation.' }, pageAccessToken);
+      return;
+    }
+
+    const prompt = args.join(' ');
+
+    try {
+      const apiUrl = `https://ccprojectapis.ddns.net/api/generate-art?prompt=${encodeURIComponent(prompt)}`;
+
+      await sendMessage(senderId, { attachment: { type: 'image', payload: { url: apiUrl } } }, pageAccessToken);
+
+    } catch (error) {
+      console.error('Error:', error);
+      await sendMessage(senderId, { text: 'Error: Could not generate image.' }, pageAccessToken);
+    }
+  }
+};
